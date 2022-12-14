28fe93b57992205674310de850b0e57aa99ea06d
Hanbi58 <hanxiaonan2012@gmail.com>
2022-12-13 18:08:46 -0800

    WIP

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,40 +2,38 @@ import styled from "styled-components";
 
 const AboutContainer = styled.section`
   min-height: 100vh;
-  width: 90vw;
+  width: 80vw;
   margin: auto;
   color: white;
   overflow: hidden;
   /* background-color: red; */
 `;
 
+const BigText = styled.div`
+  /* margin-top: 5rem; */
+  h1 {
+    font-size: ${(props) => props.theme.fontBig};
+    font-family: ${(props) => props.theme.fontA};
+    color: ${(props) => props.theme.textLight};
+    text-align: right;
+  }
+  span:nth-of-type(1) {
+    color: ${(props) => props.theme.orange};
+  }
+`;
+
 const About = () => {
   return (
     <AboutContainer>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ducimus
-        dicta nihil dolore mollitia tempore quo porro animi. Quam ullam
-        accusantium esse quidem maiores qui, impedit enim, ducimus magnam ab
-        laboriosam sint ex provident et voluptas! Doloremque voluptates sit
-        debitis doloribus nobis, ipsam magni veritatis assumenda aut! Qui hic
-        vel eos corporis! Itaque incidunt aperiam vero non, expedita ducimus
-        obcaecati quibusdam? Dolor qui laborum ipsum, aliquam numquam similique
-        aperiam reiciendis iste dolore dignissimos deleniti inventore saepe
-        asperiores quam praesentium vitae ab placeat soluta blanditiis,
-        voluptate, veniam voluptatibus perferendis dolorem? Facere sequi
-        voluptate tempora, rerum mollitia quaerat autem perferendis suscipit
-        quos nostrum dolorum! Praesentium alias veniam explicabo earum porro!
-        Aliquam cupiditate soluta sapiente, obcaecati nostrum maiores recusandae
-        quae autem, nemo error excepturi in, deserunt architecto dicta
-        reiciendis sint unde et eius fuga! Eum deleniti alias rem rerum quidem
-        odit. Autem incidunt nulla a provident voluptates excepturi ea porro
-        quod consectetur eveniet voluptas necessitatibus sint libero minus,
-        totam tenetur earum dolore quis numquam quae pariatur molestias, natus
-        reprehenderit tempore? Molestiae quaerat explicabo fugiat facere culpa.
-        Eveniet aut voluptas beatae et incidunt optio dolores in quod,
-        voluptatem, architecto accusamus aliquam obcaecati sunt maxime dolorum
-        repellendus eligendi nihil corrupti quia? Dolorum optio ea accusamus!
-      </p>
+      <p>About</p>
+      <BigText>
+        <h1 data-scroll data-scroll-speed="3">
+          Our
+          <span> Mission</span>
+          <br />
+          <span>is to Spread</span>
+        </h1>
+      </BigText>
     </AboutContainer>
   );
 };
